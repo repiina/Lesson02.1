31f4d85124af01c8a919dfb5794a0cf5e299d023
repiina <alexa6597@yandex.ru>
2024-08-04 13:00:27 +0300

    Initial Commit

---
+++ Lesson02.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson02.1
+//
+//  Created by Александра Репина on 04.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
